22dca69b7f4624bdc7e7daaf13a73633c9c86215
axle-m <alemar27@bergen.org>
2026-06-10 07:41:31 -0400

    boot1 works and loads kernel in 16 bit mode

--- src/boot/boot1.asm
+++ src/boot/boot1.asm
@@ -4,18 +4,48 @@
 KERNEL_START_SECTOR equ 6
 KERNEL_SECTORS      equ 32
 
-KERNEL_SEG equ 0xFFFF
-KERNEL_OFF equ 0x0010      ; physical 0x100000
+KERNEL_SEG equ 0x1000
+KERNEL_OFF equ 0x0000
 
-start: 
-    
+start:
 
-BOOT_DRIVE db 0
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+
+    mov si, msg
+    call print_string
+
+    ; load kernel
+
+    mov ax, KERNEL_SEG
+    mov es, ax
+    mov bx, KERNEL_OFF
+
+    mov ah, 0x02
+    mov al, KERNEL_SECTORS
+    mov ch, 0
+    mov cl, KERNEL_START_SECTOR
+    mov dh, 0
+
+    ; DL still contains boot drive from boot0
+    int 0x13
+    jc disk_error
+
+    mov si, success_msg
+    call print_string
+
+    jmp KERNEL_SEG:KERNEL_OFF
+
+disk_error:
+    mov si, disk_error_msg
+    call print_string
+    jmp $
 
 msg db "Loading kernel...",0
+success_msg db "Kernel loaded",0
+disk_error_msg db "Kernel read error",0
 
 %include "print.asm"
-%include "disk_read.asm"
 
-; pad to exactly 4 sectors
 times (2048 - ($ - $$)) db 0
