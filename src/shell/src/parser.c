1ed2f52bcdafbd0315181bf9b031c571fb4bed33
kai-ai-ver32 <kailindemer@gmail.com>
2026-06-11 01:25:36 -0400

    added parser execution logic and initial command dictionary, now just have to implement command behavior.

--- src/shell/src/parser.c
+++ src/shell/src/parser.c
@@ -2,6 +2,31 @@
 #include <stddef.h>
 #include "parser.h"
 
+extern void cmd_help(parsed_command_t *cmd);
+extern void cmd_clear(parsed_command_t *cmd);
+
+static const command_t dictionary[] = {
+    {
+        .name = "help",
+        .description = "List all available commands and their descriptions",
+        .execute = cmd_help
+    },
+    {
+        .name = "clear",
+        .description = "Clear the console input",
+        .execute = cmd_clear
+    },
+    {
+        .name = "echo",
+        .description = "Print out the command arguments",
+        .execute = cmd_echo
+    },
+};
+
+
+#define COMMAND_COUNT (sizeof(dictionary) / sizeof(dictionary[0]))
+
+
 
 void parse(char **lexed, parsed_command_t *out_cmd)
 {
@@ -28,4 +53,19 @@ void parse(char **lexed, parsed_command_t *out_cmd)
             out_cmd->args[out_cmd->arg_count++] = lexed[i];
         }
     }
+}
+
+void execute_command(parsed_command_t *cmd){
+    if(cmd->name == NULL) return;
+
+    for(int i = 0; i < COMMAND_COUNT; i++){
+        if(kstrcmp(cmd->name, dictionary[i].name) == 0){
+            dictionary[i].execute(cmd);
+            return;
+        }
+    }
+
+    print("Command \"");
+    print(cmd->name);
+    print("\" not found.\n");
 }
